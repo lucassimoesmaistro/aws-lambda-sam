8bcd36517e84a4abbea1d4d0a8243bf0cd33adf2
Your Name <lucassimoesmaistro@gmail.com>
2024-09-23 15:16:57 +0100

    policy

--- src/store_name/app.py
+++ src/store_name/app.py
@@ -1,13 +1,50 @@
 import json
+import boto3
+from boto3.dynamodb.conditions import Key
 
-# import requests
+dynamodb = boto3.resource('dynamodb')
 
+TABLE_NAME = 'NamePerCity'
 
 def lambda_handler(event, context):
+    try:
+        body = json.loads(event['body'])
+        name = body['name']
+        city = body['city']
+    except (KeyError, json.JSONDecodeError) as e:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "Invalid request format",
+                "error": str(e)
+            })
+        }
+
+    table = dynamodb.Table(TABLE_NAME)
+
+    try:
+        table.put_item(
+            Item={
+                'City': city,
+                'PersonName': name
+            }
+        )
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": "Failed to insert item into DynamoDB",
+                "error": str(e)
+            })
+        }
+
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+            "message": "Item inserted successfully",
+            "data": {
+                "name": name,
+                "city": city
+            }
+        })
     }
